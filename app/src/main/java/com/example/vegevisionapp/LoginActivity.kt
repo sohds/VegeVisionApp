4c4e3f56733273e9b3e5e936afcd4460aca05df7
Seoyeon Oh <sohtks@swu.ac.kr>
2023-07-31 06:05:43 +0900

    로그인 비밀번호 틀린 경우 토스트 + 기타 로그인 실패 사유 + 입력된 비밀번호 가린 형태로 보여주기

--- app/src/main/java/com/example/vegevisionapp/LoginActivity.kt
+++ app/src/main/java/com/example/vegevisionapp/LoginActivity.kt
@@ -2,6 +2,8 @@ package com.example.vegevisionapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -9,11 +11,10 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
-    lateinit var emailEt: EditText
-    lateinit var passwordEt: EditText
-    lateinit var loginBtn: Button
-
-    lateinit var auth: FirebaseAuth
+    private lateinit var emailEt: EditText
+    private lateinit var passwordEt: EditText
+    private lateinit var loginBtn: Button
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,8 +23,30 @@ class LoginActivity : AppCompatActivity() {
         auth = FirebaseAuth.getInstance()
 
         emailEt = findViewById(R.id.editid)
-        passwordEt = findViewById(R.id.editpw)
+        passwordEt = findViewById(R.id.passwordEditText)
         loginBtn = findViewById(R.id.loginBtn)
+
+        // 비밀번호 입력 창에 TextWatcher를 등록
+        passwordEt.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // 이전 텍스트 값
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // 현재 텍스트 값
+                val currentText = s.toString()
+
+                // 입력된 비밀번호를 가려진 형태로 보여줌
+                passwordEt.removeTextChangedListener(this)
+                passwordEt.setText(currentText)
+                passwordEt.setSelection(passwordEt.length())
+                passwordEt.addTextChangedListener(this)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // 입력이 완료된 후 작업
+            }
+        })
 
         loginBtn.setOnClickListener {
             val email = emailEt.text.toString()
@@ -33,32 +56,26 @@ class LoginActivity : AppCompatActivity() {
                 // 이메일 또는 비밀번호가 비어있을 경우
                 Toast.makeText(this, "이메일 또는 비밀번호를 입력해주세요.", Toast.LENGTH_SHORT).show()
             } else {
-                auth.createUserWithEmailAndPassword(email, password)
+                auth.signInWithEmailAndPassword(email, password)
                     .addOnCompleteListener { result ->
                         if (result.isSuccessful) {
-                            Toast.makeText(this, "회원가입이 완료되었습니다.", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(this, "로그인 성공", Toast.LENGTH_SHORT).show()
                             val intent = Intent(this, MainActivity::class.java)
                             startActivity(intent)
+                            finish() // 로그인 액티비티 종료
                         } else {
-                            login(email, password)
+                            // 로그인 실패 시 오류 메시지 확인
+                            val error = result.exception?.message
+                            if (error == "The password is invalid or the user does not have a password.") {
+                                // 비밀번호가 틀린 경우
+                                Toast.makeText(this, "비밀번호가 틀렸습니다.", Toast.LENGTH_SHORT).show()
+                            } else {
+                                // 기타 로그인 실패 사유
+                                Toast.makeText(this, "로그인 실패: $error", Toast.LENGTH_SHORT).show()
+                            }
                         }
                     }
             }
         }
-
-    }
-
-    fun login(email:String, password:String){
-        auth.signInWithEmailAndPassword(email,password) // 로그인
-            .addOnCompleteListener {
-                    result->
-                if(result.isSuccessful){
-                    Toast.makeText(this,"로그인 성공",Toast.LENGTH_SHORT).show()
-                    val intent = Intent(this, MainActivity::class.java)
-                    startActivity(intent)
-
-                    finish() // 로그인 액티비티 종료
-                }
-            }
     }
 }

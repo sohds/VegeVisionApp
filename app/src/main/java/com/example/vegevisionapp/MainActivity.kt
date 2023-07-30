1287e9972df88d93a3ecc84a72cfebafc0c20507
Seoyeon Oh <sohtks@swu.ac.kr>
2023-07-31 02:06:06 +0900

    카메라 권한 토스트 설정 (재묻기)

--- app/src/main/java/com/example/vegevisionapp/MainActivity.kt
+++ app/src/main/java/com/example/vegevisionapp/MainActivity.kt
@@ -6,12 +6,10 @@ import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
-import android.widget.ImageView
-import android.widget.Toolbar
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import java.io.ByteArrayOutputStream
@@ -20,7 +18,6 @@ class MainActivity : BaseActivity() {
 
     lateinit var GalleryBtn: Button
     lateinit var CameraBtn: Button
-    lateinit var imageView: ImageView
 
     lateinit var AboutBtn: ImageButton
     lateinit var homeButton: ImageButton
@@ -69,9 +66,10 @@ class MainActivity : BaseActivity() {
             startActivity(intent)
         }
 
-        // homeButton 클릭 이벤트 처리 - MainActivity(현재 액티비티)로 이동 (또는 아무 동작 안 하게 설정)
+        // homeButton 클릭 이벤트 처리 - MainActivity(현재 액티비티)로 이동
         homeButton.setOnClickListener {
-            // Do nothing or you can add custom behavior here
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
 
         // TipBtn 클릭 이벤트 처리 - TipActivity로 이동
@@ -81,10 +79,18 @@ class MainActivity : BaseActivity() {
         }
     }
 
-    private val REQUEST_GALLERY_PERMISSION = 1002
     private val REQUEST_CAMERA_PERMISSION = 1003
     private val REQUEST_IMAGE_GALLERY = 101
     private val REQUEST_IMAGE_CAPTURE = 102
+
+    // 갤러리 앱에 권한 요청을 하지 않아도 되는 이유:
+    // 갤러리 앱은 이미 시스템 레벨에서 이미지를 가져오기 위해 필요한 권한을 가지고 있을 수 있음
+    // 따라서 우리 앱에서 갤러리 앱을 실행할 때, 갤러리 앱은 이미 권한이 허용되어 있으므로
+    // 권한을 다시 요청하지 않고 이미지를 선택하는 작업이 가능
+
+
+    // 반면 카메라 앱은 일반적으로 런타임에 권한을 요청해야 하기 때문에
+    // 사용자에게 권한을 요청하는 코드를 구현해야 함!
 
     private fun requestCameraPermission() {
         // 카메라 권한 요청
@@ -120,10 +126,23 @@ class MainActivity : BaseActivity() {
                 if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     openCamera()
                 } else {
-                    // 권한을 거부한 경우 처리
+                    // 카메라 권한이 거부된 경우
+                    showCameraPermissionDeniedToast()
                 }
             }
         }
+    }
+
+    private fun showCameraPermissionDeniedToast() {
+        // 카메라 권한이 거부된 경우에 토스트 메시지를 보여줍니다.
+        Toast.makeText(this, "카메라의 권한을 허용해야 사용 가능합니다.", Toast.LENGTH_SHORT).show()
+
+        // 권한을 다시 요청합니다.
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.CAMERA),
+            REQUEST_CAMERA_PERMISSION
+        )
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {

8acfd064167bc3caeacf876662bb3203b91515b1
Seoyeon Oh <sohtks@swu.ac.kr>
2023-07-31 02:12:39 +0900

    class명 바꿈 + 변수 명명 규칙에 맞게 변경

--- app/src/main/java/com/example/vegevisionapp/StartActivity.kt
+++ app/src/main/java/com/example/vegevisionapp/StartActivity.kt
@@ -5,18 +5,18 @@ import android.os.Bundle
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
-class OnboardingActivity : AppCompatActivity() {
+class StartActivity : AppCompatActivity() {
 
-    lateinit var AppleBtn: ImageButton
+    lateinit var appleBtn: ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.start_page)
 
-        AppleBtn = findViewById(R.id.imageView)
+        appleBtn = findViewById(R.id.imageView)
 
         // AboutBtn 클릭 이벤트 처리 - AboutActivity로 이동 (현재 액티비티)
-        AppleBtn.setOnClickListener {
+        appleBtn.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
